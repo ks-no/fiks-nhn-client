c7f8a86e199e961b28ec2d741a7724e42d13c7b8
Anders Flemmen <anders.flemmen@ks.no>
2025-06-03 14:41:11 +0200

    Use local copy of xmldsig xsd (#30)

--- msh-client/src/main/kotlin/no/ks/fiks/nhn/edi/XmlContext.kt
+++ msh-client/src/main/kotlin/no/ks/fiks/nhn/edi/XmlContext.kt
@@ -6,6 +6,7 @@ import jakarta.xml.bind.Unmarshaller
 import jakarta.xml.bind.util.JAXBSource
 import no.kith.xmlstds.base64container.Base64Container
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead
+import org.w3._2000._09.xmldsig_.ObjectFactory
 import javax.xml.XMLConstants
 import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.SchemaFactory
@@ -14,10 +15,17 @@ import no.kith.xmlstds.dialog._2013_01_23.Dialogmelding as Dialogmelding1_1
 
 object XmlContext {
 
-    private val context = JAXBContext.newInstance(MsgHead::class.java, Base64Container::class.java, Dialogmelding1_0::class.java, Dialogmelding1_1::class.java)
+    private val context = JAXBContext.newInstance(
+        MsgHead::class.java,
+        Base64Container::class.java,
+        Dialogmelding1_0::class.java,
+        Dialogmelding1_1::class.java,
+        ObjectFactory::class.java,
+    )
     private val headSchema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
         .newSchema(
             arrayOf(
+                StreamSource(ClassLoader.getSystemResourceAsStream("xsd/xmldsig-core-schema.xsd")),
                 StreamSource(ClassLoader.getSystemResourceAsStream("xsd/kith.xsd")),
                 StreamSource(ClassLoader.getSystemResourceAsStream("xsd/felleskomponent1.xsd")),
                 StreamSource(ClassLoader.getSystemResourceAsStream("xsd/MsgHead-v1_2.xsd")),
