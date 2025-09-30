a74856f856f6823aecc8639895256b38f64d4811
Anders Flemmen <anders.flemmen@ks.no>
2025-09-30 09:31:20 +0200

    Extra send apprec tests

--- msh-client/src/test/kotlin/no/ks/fiks/nhn/examples/SendApplicationReceiptTest.kt
+++ msh-client/src/test/kotlin/no/ks/fiks/nhn/examples/SendApplicationReceiptTest.kt
@@ -13,17 +13,48 @@ import java.time.OffsetDateTime
 import java.util.*
 
 class SendApplicationReceiptTest : StringSpec({
-    "Sending an example application receipt" {
+    "Sending an OK example application receipt" {
         val client = mockk<ClientWithFastlegeLookup> { coEvery { sendApplicationReceipt(any(), any()) } returns UUID.randomUUID() }
         client.sendApplicationReceipt(
             OutgoingApplicationReceipt(
                 acknowledgedId = UUID.randomUUID(), // Id of the received message that this app rec is acknowledging
                 senderHerId = 1234,
-                status = StatusForMottakAvMelding.AVVIST,
-                errors = listOf( // Not allowed when status is OK, required otherwise
+                status = StatusForMottakAvMelding.OK,
+            )
+        )
+    }
+
+    "Sending an OK_FEIL_I_DELMELDING example application receipt" {
+        val client = mockk<ClientWithFastlegeLookup> { coEvery { sendApplicationReceipt(any(), any()) } returns UUID.randomUUID() }
+        client.sendApplicationReceipt(
+            OutgoingApplicationReceipt(
+                acknowledgedId = UUID.randomUUID(),
+                senderHerId = 1234,
+                status = StatusForMottakAvMelding.OK_FEIL_I_DELMELDING,
+                errors = listOf(
                     OutgoingApplicationReceiptError(
                         FeilmeldingForApplikasjonskvittering.ANNEN_FEIL,
                         "Details describing the error",
+                    ),
+                    OutgoingApplicationReceiptError(
+                        FeilmeldingForApplikasjonskvittering.PASIENT_MANGLER_NAVN,
+                    ),
+                ),
+            )
+        )
+    }
+
+    "Sending an AVVIST example application receipt" {
+        val client = mockk<ClientWithFastlegeLookup> { coEvery { sendApplicationReceipt(any(), any()) } returns UUID.randomUUID() }
+        client.sendApplicationReceipt(
+            OutgoingApplicationReceipt(
+                acknowledgedId = UUID.randomUUID(),
+                senderHerId = 1234,
+                status = StatusForMottakAvMelding.AVVIST,
+                errors = listOf(
+                    OutgoingApplicationReceiptError(
+                        FeilmeldingForApplikasjonskvittering.UGYLDIG_XML,
+                        "XML is invalid",
                     )
                 ),
             )
