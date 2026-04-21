d2e4fd5de3da1f34ea44daadba44bc4bee1e2262
Anders Flemmen <anders.flemmen@ks.no>
2026-04-21 11:52:04 +0200

    Handle incoming messages of type DIALOG_NOTAT

--- msh-client/src/main/kotlin/no/ks/fiks/hdir/MeldingensFunksjon.kt
+++ msh-client/src/main/kotlin/no/ks/fiks/hdir/MeldingensFunksjon.kt
@@ -5,9 +5,14 @@ enum class MeldingensFunksjon(
     override val verdi: String,
     override val navn: String,
 ) : KodeverkVerdi {
+
+    // Dialogmelding 1.0
     DIALOG_FORESPORSEL("DIALOG_FORESPORSEL", "Forespørsel"),
-    DIALOG_HELSEFAGLIG("DIALOG_HELSEFAGLIG", "Helsefaglig dialog"),
-    DIALOG_SVAR("DIALOG_SVAR", "Svar på forespørsel");
+    DIALOG_SVAR("DIALOG_SVAR", "Svar på forespørsel"),
+    DIALOG_NOTAT("DIALOG_NOTAT", "Notat"), // Removed in the latest revision of the standard, but still supported for backward compatibility
+
+    // Dialogmelding 1.1
+    DIALOG_HELSEFAGLIG("DIALOG_HELSEFAGLIG", "Helsefaglig dialog");
 
     override val kodeverk: String = "2.16.578.1.12.4.1.1.8279"
 }
